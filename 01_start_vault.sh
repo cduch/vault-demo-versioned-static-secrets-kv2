a9f733e488db78fb50eb8e1dfa1b69371d257022
Carsten Duch <carsten@hashicorp.com>
2021-08-06 13:41:11 +0200

    changed to vault enterprise 1.8.1

--- 01_start_vault.sh
+++ 01_start_vault.sh
@@ -9,7 +9,7 @@ docker network rm dev-network
 #start Vault in dev mode on port 8200
 docker network create dev-network
 
-docker run --name vault-demo-vault --network dev-network -p 8200:8200 hashicorp/vault-enterprise:1.7.3_ent server -dev -dev-root-token-id="root" &
+docker run --name vault-demo-vault --network dev-network -p 8200:8200 -e VAULT_LICENSE="${VAULT_LICENSE}" hashicorp/vault-enterprise:1.8.1_ent server -dev -dev-root-token-id="root" &
 
 sleep 10
 
